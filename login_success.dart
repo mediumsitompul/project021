8a164ee11a4ac6b32ea36c9114c0f60ee6d83264
mediumsitompul <44793858+mediumsitompul@users.noreply.github.com>
2023-04-30 21:13:20 +0700

    Create login_success.dart

---
+++ login_success.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginSuccess extends StatelessWidget {
+  const LoginSuccess({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Center(child: Text("Hellooo"))),
+      body: const Center(child: Text('Welcome to MyHomePage')),
+    );
+  }
+}
